fb40cd4e4032ca0ecc01e929fe1aa0adb28eab13
TobiasK53 <42980223+TobiasK53@users.noreply.github.com>
2018-11-30 13:56:24 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,9 +5,19 @@ function setup() {
 	man=new Person();
 }
 
+var x;
+x=-100;
 function draw() {
 	background(200);
+  
+  var force = createVector(-0.01,0);
+  man.applyForce(force);
+  
+	translate(-man.pos.x,0);
 	man.update();
 	man.display();
+	
+	fill(0,0,0);
+	rect(350,height-50,50,70);
 }
 	
